01693c80bfc7f0e46747176920f0dc30df840554
Zygimantas <zygimantas1997@gmail.com>
2020-09-28 17:22:55 +0300

    TheBattleApiRequestExamples

---
+++ Services/IdentityService.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBattleShipClient.Services
+{
+    public class IdentityService
+    {
+        public const string BASE_URL = "https://thebattleapi.azurewebsites.net/api/v1/Identity/";
+
+        public static async Task<AuthenticationResponse> Register(UserRequest request)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            Uri uri = new Uri(BASE_URL + "register");
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.PostAsync(uri, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<AuthenticationResponse>(responseString);
+            }
+        }
+
+        public static async Task<AuthenticationResponse> Login(UserRequest request)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            Uri uri = new Uri(BASE_URL + "login");
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.PostAsync(uri, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<AuthenticationResponse>(responseString);
+            }
+        }
+    }
+
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+
+    public class UserRequest
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Services/RoomsService.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBattleShipClient.Services
+{
+    public class RoomsService
+    {
+        public const string BASE_URL = "https://thebattleapi.azurewebsites.net/api/v1/Rooms/";
+
+        public static async Task<RoomResponse> CreateRoom(string token, RoomRequest request)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            Uri uri = new Uri(BASE_URL);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await client.PostAsync(uri, content);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<RoomResponse>(jsonResponse);
+            }
+        }
+        
+        public static async Task<RoomResponse> JoinRoom(string token, string roomId)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(new RoomRequest());
+            HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            Uri uri = new Uri(BASE_URL + roomId);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await client.PutAsync(uri, content);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<RoomResponse>(jsonResponse);
+            }
+        }
+        
+        /*
+        public Task<string> IsMyTurn(string token)
+        {
+
+        }
+        */
+        public class RoomRequest
+        {
+            public int Size { get; set; }
+        }
+        public class RoomResponse
+        {
+            public string Id { get; set; }
+            public int Size { get; set; }
+        }
+    }
+}
