75ca92ddb5f0f721771efc427d64d592871c3452
rob_mill <roberta.milasauskaite@gmail.com>
2020-10-27 19:49:47 +0200

    Add Adapter design pattern

---
+++ Models/Weapons/Factories/Adapter/MineAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBattleShipClient.Services;
+using static TheBattleShipClient.Services.WeaponsService;
+
+namespace TheBattleShipClient.Models.Weapons.Factories.Adapter
+{
+    class MineAdapter : WeaponFactory
+    {
+        public MineAdapter(string token, string roomId)
+           : base(token, roomId)
+        {
+        }
+        public override async Task<Weapon> CreateWeapon(int x, int y)
+        {
+            var weaponRequest = MineFactory.Create(x, y);
+            var mine = await MineFactory.Shot(weaponRequest, _token, _roomId);
+            return mine;
+        }
+    }
+}
