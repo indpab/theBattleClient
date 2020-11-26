515d0fdd43b70c8f2004999c2cfbd921b5570147
rob_mill <roberta.milasauskaite@gmail.com>
2020-11-26 07:12:23 +0200

    Add FleetTemplate

---
+++ Models/Ships/Template/DestroyerFleetTemplate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+using TheBattleShipClient.Models.Ships;
+using TheBattleShipClient.Models.Ships.Builder;
+using TheBattleShipClient.Models.Ships.Factories;
+
+namespace TheBattleShipClient.Models
+{
+    public class DestroyerFleetTemplate : FleetTemplate
+    {
+        public DestroyerFleetTemplate(string token, string roomId)
+            : base(token, roomId)
+        {
+        }
+
+        protected override ShipGroup CreateAtomicShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_atomicShipFactory.CreateDestroyer(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Atomic Destroyer",
+                    IsSubmarine = false,
+                    Size = 4
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+
+        protected override ShipGroup CreateLargeShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_largeShipFactory.CreateDestroyer(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Large Destroyer",
+                    IsSubmarine = false,
+                    Size = 3
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+
+        protected override ShipGroup CreateMediumShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_mediumShipFactory.CreateDestroyer(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Medium Destroyer",
+                    IsSubmarine = false,
+                    Size = 2
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+
+        protected override ShipGroup CreateSmallShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_smallShipFactory.CreateDestroyer(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Small Destroyer",
+                    IsSubmarine = false,
+                    Size = 1
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+    }
+}
---
+++ Models/Ships/Template/SubmarineFleetTemplate.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheBattleShipClient.Models.Ships.Factories;
+
+namespace TheBattleShipClient.Models.Ships.Builder
+{
+    public class SubmarineFleetTemplate : FleetTemplate
+    {
+        public SubmarineFleetTemplate(string token, string roomId)
+            : base(token, roomId)
+        {
+        }
+        protected override ShipGroup CreateAtomicShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_atomicShipFactory.CreateSubmarine(0, 0, true)); 
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Atomic Submarine",
+                    IsSubmarine = true,
+                    Size = 4
+                },
+                Ships = ships
+            };
+
+            return shipGroup;
+        }
+
+        protected override ShipGroup CreateLargeShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_largeShipFactory.CreateSubmarine(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Large Submarine",
+                    IsSubmarine = true,
+                    Size = 3
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+
+        protected override ShipGroup CreateMediumShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_mediumShipFactory.CreateSubmarine(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Medium Submarine",
+                    IsSubmarine = true,
+                    Size = 2
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+
+        protected override ShipGroup CreateSmallShips(int count)
+        {
+            var ships = new List<Ship>();
+            for (int i = 0; i < count; i++)
+            {
+                ships.Add(_smallShipFactory.CreateSubmarine(0, 0, true));
+            }
+            var shipGroup = new ShipGroup
+            {
+                Limit = count,
+                Count = count,
+                ShipType = new ShipType
+                {
+                    Name = "Small Submarine",
+                    IsSubmarine = true,
+                    Size = 1
+                },
+                Ships = ships
+            };
+            return shipGroup;
+        }
+    }
+}
